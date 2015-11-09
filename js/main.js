e93d819f175dc0ee58d8d6abd14eb9bb4326d06a
Jakub <jakub.vaclavik@firma.seznam.cz>
2015-11-09 18:45:38 +0100

    Json Obvody rozdělen na Venkov/mesto

--- js/main.js
+++ js/main.js
@@ -9,20 +9,23 @@ require.config({
         "data/hranice": "../data/hranice2015.json",
         "data/obchod": "../data/obchod2015.json",
         "data/obvod": "../data/obvod2015.json",
+        "data/obvodMesto": "../data/obvod2015_mesto.json",
+        "data/obvodVenkov": "../data/obvod2015_venkov.json",
 
 	},
 	waitSeconds: 15
 });
 
 //load and init 
-require(['app', 'map', 'treeList', 'text!sidebarHtml', 'utils', "text!data/obvod", "text!data/obchod", "text!data/hranice"], 
-	function(app, map ,treeList, barTmpl, utils, obvod, obchod, hranice){
+require(['app', 'map', 'treeList', 'text!sidebarHtml', 'utils', "text!data/obvod", "text!data/obchod", "text!data/hranice", "text!data/obvodMesto", "text!data/obvodVenkov"], 
+	function(app, map ,treeList, barTmpl, utils, obvod, obchod, hranice, mesto, venkov){
 		map.init();
 
         utils.loadTemplate(barTmpl, 'sidebar', app);
 
         var vrstvy = [];
-        vrstvy.push(JSON.parse(obvod));
+        vrstvy.push(JSON.parse(mesto));
+        vrstvy.push(JSON.parse(venkov));
         vrstvy.push(JSON.parse(obchod)); 
         vrstvy.push(JSON.parse(hranice)); 
 
